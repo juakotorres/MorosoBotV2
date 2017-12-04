3774bef7138e54100747d03e3c31a562adc57bae
Sergio-P <elxsergio@gmail.com>
2017-12-04 17:03:46 -0300

    Add command paguenctm and misdeudas

--- src/main/scala/MainBot.scala
+++ src/main/scala/MainBot.scala
@@ -1,22 +1,56 @@
 import info.mukel.telegrambot4s.api.declarative.Commands
 import info.mukel.telegrambot4s.api.{Polling, TelegramBot}
+import info.mukel.telegrambot4s.methods.ParseMode
 
 import scala.io.Source
 
 object MainBot extends TelegramBot with Polling with Commands {
-  // Use 'def' or 'lazy val' for the token, using a plain 'val' may/will
-  // lead to initialization order issues.
-  // Fetch the token from an environment variable or untracked file.
-  lazy val token = scala.util.Properties
-    .envOrNone("BOT_TOKEN")
-    .getOrElse(Source.fromFile("bot.token").getLines().mkString)
+    lazy val token: String = scala.util.Properties
+        .envOrNone("BOT_TOKEN")
+        .getOrElse(Source.fromFile("bot.token").getLines().mkString)
 
-  onCommand('start) { implicit msg => reply(  "Bueno cabros se viene la hora de pagar. \n")}
+    onCommand('start) { implicit msg =>
+        reply("Bueno cabros se viene la hora de pagar. \n")
+    }
 
-  onCommand('help) { implicit msg => reply("Usar los comandos para agregar las deudas pendientes. \n" +
-                                            "Agregar deuda con commando /ledebo o /medebe @tagdeudor monto\n" +
-                                            "Saldar deuda o ya pago usar commando /mepago @tagdeudor monto\n" +
-                                            "Mostrar deudas pendientes del usuario usar commando /misdeudas\n" +
-                                            "Para ver mis deudores /paguenctm")}
+    onCommand('help) { implicit msg =>
+        reply("Usar los comandos para agregar las deudas pendientes. \n" +
+            "Agregar deuda con commando /ledebo o /medebe @tagdeudor monto\n" +
+            "Saldar deuda o ya pago usar commando /mepago @tagdeudor monto\n" +
+            "Mostrar deudas pendientes del usuario usar commando /misdeudas\n" +
+            "Para ver mis deudores /paguenctm")
+    }
+
+    onCommand('misdeudas) { implicit msg =>
+        var builder = ""
+        var sum = 0
+        msg.from.foreach { user =>
+            builder += s"Deudas de @${user.username.getOrElse("")}\n"
+            DBInterface.getUserDebts(user.username.getOrElse("")).foreach { debt =>
+                builder += s"*${debt.amount}* a @${debt.user_to} - ${debt.reason}\n"
+                sum += debt.amount
+            }
+            builder += s"Total: *$sum*\n"
+        }
+        if(sum == 0)
+            builder += "No hay deudas m3n"
+        reply(builder, Some(ParseMode.Markdown))
+    }
+
+    onCommand('paguenctm) { implicit msg =>
+        var builder = ""
+        var sum = 0
+        msg.from.foreach { user =>
+            builder += s"Deudas a @${user.username.getOrElse("")}\n"
+            DBInterface.getUserIncomes(user.username.getOrElse("")).foreach { debt =>
+                builder += s"*${debt.amount}* de @${debt.user_from} - ${debt.reason}\n"
+                sum += debt.amount
+            }
+            builder += s"Total: *$sum*\n"
+        }
+        if(sum == 0)
+            builder += "No hay money m3n"
+        reply(builder, Some(ParseMode.Markdown))
+    }
 
 }
